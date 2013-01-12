c91c7e49fd7edf8b58797c099ce993ebb5a519d6
unknown <dzhariy@dz.(none)>
2013-01-13 01:40:01 +0200

    Updated with static PO

---
+++ 2013-02-09__ATDays.Kyiv/src/DemoTestWebdriver/DemoTest/StaticPageobject/MyPages.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace DemoTest.StaticPageobject
+{
+    public static class MyPages
+    {
+        public static RemoteWebDriver Driver 
+        { 
+                 get { return WebBrowser.Driver; }
+        }
+
+        public static class MainPage
+        {
+            public static void Open()
+            {
+                Driver.Navigate().GoToUrl(@"http://en.wikipedia.org");
+            }
+
+            public static void GoToDonatePage()
+            {
+                var lnkDonate = Driver.FindElementByCssSelector(@"a[title='Support us']");
+                lnkDonate.Click();
+            }
+
+        }
+        public static class DonatePage
+        {
+            public static void Donate_50_UAH_Using_Debit_Card()
+            {
+
+                WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(30));
+                                
+                var rbtnDonate50 = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(@"input[name='amount'][value='50']")));
+                rbtnDonate50.Click();
+
+                var btnMakeDonation = Driver.FindElementByCssSelector(@"input[value='Donate by credit/debit card']");
+                btnMakeDonation.Click();
+
+            }
+        }
+        public static class DonationPaymentsForm
+        {
+            public static void FillDonationForm(string firstName, string lastName, string street, 
+                                                string postalCode, string city, string emailAddress,
+                                                string cardNumber, string expirationDateMonth, string expirationDateYear,
+                                                string securityCode)
+            {
+                // Donation Payments
+                var txtFirstName = Driver.FindElementByName("fname");
+                var txtLastName = Driver.FindElementByName("lname");
+                var txtStreet = Driver.FindElementByName("street");
+                var txtPostalCode = Driver.FindElementByName("zip");
+                var txtCity = Driver.FindElementByName("city");
+                var txtEmailAddress = Driver.FindElementByName("emailAdd");
+                var rbtnCardVisa = Driver.FindElementByCssSelector(@"input[name='cardtype'][value='visa']");
+
+
+                txtFirstName.Clear();
+                txtFirstName.SendKeys(firstName);
+
+                txtLastName.Clear();
+                txtLastName.SendKeys(lastName);
+
+                txtStreet.Clear();
+                txtStreet.SendKeys(street);
+
+                txtPostalCode.Clear();
+                txtPostalCode.SendKeys(postalCode);
+
+                txtCity.Clear();
+                txtCity.SendKeys(city);
+
+                txtEmailAddress.Clear();
+                txtEmailAddress.SendKeys(emailAddress);
+
+                rbtnCardVisa.Click();
+
+                WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(30));
+
+
+                var frameCardNumber = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(@"iframe[src^='https://na.gcsip.com/orb/']")));
+
+                Driver.SwitchTo().Frame(frameCardNumber);
+
+                var txtName = wait.Until(ExpectedConditions.ElementIsVisible(By.Name("CREDITCARDNUMBER")));
+
+                txtName.Clear();
+                txtName.SendKeys(cardNumber);
+
+                var ddlMonthSelect = new SelectElement(Driver.FindElementByName(@"EXPIRYDATE_MM"));
+                var ddlYearSelect = new SelectElement(Driver.FindElementByName(@"EXPIRYDATE_YY"));
+                var txtSecurityCode = Driver.FindElementByName(@"CVV");
+                var btnContinue = Driver.FindElementById("btnSubmit");
+
+                ddlMonthSelect.SelectByText(expirationDateMonth);
+                ddlYearSelect.SelectByText(expirationDateYear);
+                txtSecurityCode.SendKeys(securityCode);
+
+                btnContinue.SendKeys(Keys.Enter);
+
+                Driver.SwitchTo().DefaultContent();
+
+            }
+        }
+        public static class PaymentResultPage
+        {
+            public static void WaitUntilExists()
+            {
+                WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(30));
+                wait.Until(ExpectedConditions.TitleContains(@"Donate-error - Payments"));
+            }
+
+            public static string GetResultHeaderText()
+            {
+                var lblFirstHeader = Driver.FindElementById(@"firstHeading");
+                return lblFirstHeader.Text;
+            }
+
+        }
+    }
+}
--- 2013-02-09__ATDays.Kyiv/src/DemoTestWebdriver/DemoTest/UnitTest1.cs
+++ 2013-02-09__ATDays.Kyiv/src/DemoTestWebdriver/DemoTest/UnitTest1.cs
@@ -6,19 +6,23 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Remote;
-
+using OpenQA.Selenium.Support.UI;
+using System.Diagnostics;
 
 namespace DemoTest
 {
     [TestClass]
     public class UnitTest1
     {
-        public void Wikipedia+Smart_Search_Test()
+        public void Wikipedia_Smart_Search_Test()
         {
             RemoteWebDriver driver = new InternetExplorerDriver();
-            driver.Navigate().GoToUrl("http://en.wikipedia.org/wiki/Main_Page");
-            driver.FindElementByCssSelector(@"div#simpleSearch").SendKeys("Webdriver Selenium");
-            Assert.AreEqual("http://en.wikipedia.org/wiki/WebDriver#Selenium_WebDriver", driver.Url);
+            driver.Navigate()
+                  .GoToUrl("http://en.wikipedia.org/wiki/Main_Page");
+            driver.FindElementByCssSelector(@"div#simpleSearch")
+                  .SendKeys("Webdriver Selenium");
+            Assert.AreEqual("http://en.wikipedia.org/wiki/WebDriver#Selenium_WebDriver"
+                           , driver.Url);
         }
 
 
@@ -34,7 +38,7 @@ namespace DemoTest
             Wait();
 
 
-            driver.FindElement(By.XPath("//a[text()='Log On']")).Click()
+            driver.FindElement(By.XPath("//a[text()='Log On']")).Click();
             
 
             // Click on the Log On link.
@@ -43,12 +47,134 @@ namespace DemoTest
             Wait();
 
             // Check that the page title is that of the User/Logon page.
-            Assert.That(driver.Title, Is.EqualTo("Task Tracker - Log On"));
+            Assert.AreEqual(driver.Title, "Task Tracker - Log On");
 
             // Enter the username and password.
             IWebElement userName = driver.FindElement(By.Id("UserName"));
             userName.SendKeys("testuser");
             // ... 
         }
+
+        private void Wait()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void Donate_test()
+        {
+            RemoteWebDriver driver = new InternetExplorerDriver();
+            // Navigate to the base url.
+            driver.Navigate().GoToUrl(@"http://en.wikipedia.org");
+
+            // Main Page
+            var lnkDonate = driver.FindElementByCssSelector(@"a[title='Support us']");
+            lnkDonate.Click();
+
+            // Donate Page
+            var rbtnDonate50 = driver.FindElementByCssSelector(@"input[name='amount'][value='50']");
+            rbtnDonate50.Click();
+
+            var btnMakeDonation = driver.FindElementByCssSelector(@"input[value='Donate by credit/debit card']");
+            btnMakeDonation.Click();
+
+            // Donation Payments
+            var txtFirstName = driver.FindElementByName("fname");
+            var txtLastName = driver.FindElementByName("lname");
+            var txtStreet = driver.FindElementByName("street");
+            var txtPostalCode = driver.FindElementByName("zip");
+            var txtCity = driver.FindElementByName("city");
+            var txtEmailAddress = driver.FindElementByName("emailAdd");
+            var rbtnCardVisa = driver.FindElementByCssSelector(@"input[name='cardtype'][value='visa']");
+
+
+            txtFirstName.Clear();
+            txtFirstName.SendKeys("Vasya");
+
+            txtLastName.Clear();
+            txtLastName.SendKeys("Pupkin");
+
+            txtStreet.Clear();
+            txtStreet.SendKeys("Blah blah blah street");
+
+            txtPostalCode.Clear();
+            txtPostalCode.SendKeys("8577");
+
+            txtCity.Clear();
+            txtCity.SendKeys("My Str");
+
+            txtEmailAddress.Clear();
+            txtEmailAddress.SendKeys("valid@example.com");
+
+            rbtnCardVisa.Click();
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+
+            
+            var frameCardNumber=wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(@"iframe[src^='https://na.gcsip.com/orb/']")));
+
+            driver.SwitchTo().Frame(frameCardNumber);
+
+            var txtName = wait.Until(ExpectedConditions.ElementIsVisible(By.Name("CREDITCARDNUMBER")));
+
+            txtName.Clear();
+            txtName.SendKeys("4556767453966453");
+
+            var ddlMonthSelect = new SelectElement(driver.FindElementByName(@"EXPIRYDATE_MM"));
+            var ddlYearSelect = new SelectElement(driver.FindElementByName(@"EXPIRYDATE_YY"));
+            var txtSecurityCode = driver.FindElementByName(@"CVV");
+            var btnContinue = driver.FindElementById("btnSubmit");
+
+            ddlMonthSelect.SelectByText("05");
+            ddlYearSelect.SelectByText("15");
+            txtSecurityCode.SendKeys("555");
+
+            Assert.IsTrue(btnContinue.Displayed, "There is a Continue button");
+            
+            btnContinue.SendKeys(Keys.Enter);
+
+            driver.SwitchTo().DefaultContent();
+
+            wait.Until(ExpectedConditions.TitleContains(@"Donate-error - Payments"));
+
+            var lblFirstHeader = driver.FindElementById(@"firstHeading");
+
+            Assert.AreEqual("Donate-error", lblFirstHeader.Text);
+
+        }
+
+
+        [TestMethod]
+        public void XPath_vs_CSS_test_yandex()
+        {
+            RemoteWebDriver driver = new InternetExplorerDriver();
+            // Navigate to the base url.
+            driver.Navigate().GoToUrl(@"http://yandex.ru");
+
+            Stopwatch sw2 = new Stopwatch();
+            sw2.Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                driver.FindElementByXPath("//div");
+            }
+            sw2.Stop();
+
+            Stopwatch sw1 = new Stopwatch();
+            sw1.Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                driver.FindElementsByCssSelector("div");
+            }
+            sw1.Stop();
+
+
+
+            Console.WriteLine("CSS: " + sw1.ElapsedMilliseconds);
+            Console.WriteLine("XPath: " + sw2.ElapsedMilliseconds);
+
+        }
+
     }
 }
